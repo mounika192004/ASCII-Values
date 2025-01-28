fccf268d0044ab47b542a7ef41b14edae5305cd1
Mounika <nagargojemounika@gmail.com>
2025-01-28 20:55:23 +0530

    Add files via upload

---
+++ ASCIIvalue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ASCIIvalue {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    char a = sc.next().charAt(0);
+    int b = a;
+    System.out.println("Ascii value of a: "+(b));    
+    }
+}
